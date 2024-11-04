39becd29a747b8dca70fe73cb6123e93963c9f19
Yannick <yannick.frei@student.ipso.ch>
2024-11-04 11:47:31 +0100

    Update support.js

--- Ark Server/js/support.js
+++ Ark Server/js/support.js
@@ -4,6 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     if (form) {
         form.addEventListener('submit', (event) => {
             event.preventDefault(); // Verhindert das Abschicken des Formulars
+            
+            // Erfasst die Eingabewerte der Felder
+            const vorname = document.getElementById('vname').value;
+            const nachname = document.getElementById('nname').value;
+            const email = document.getElementById('email').value;
+            const kategorie = document.getElementById('kategorie').value;
+            const nachricht = document.getElementById('nachricht').value;
+
+            // Ausgabe der Eingabewerte in der Konsole
+            console.log(`Vorname: ${vorname}`);
+            console.log(`Nachname: ${nachname}`);
+            console.log(`E-Mail: ${email}`);
+            console.log(`Kategorie: ${kategorie}`);
+            console.log(`Nachricht: ${nachricht}`);
+
             alert('Danke für die Rückmeldung!');
             form.reset(); // Setzt das Formular zurück
         });
